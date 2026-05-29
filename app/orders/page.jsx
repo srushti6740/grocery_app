1b5e762c570190b4c2bdecf522c2dd7afcf32285
srushti6740 <sshah.dev@theoceanstudios.in>
2026-05-29 11:01:39 +0530

    changed

--- app/orders/page.jsx
+++ app/orders/page.jsx
@@ -1,23 +1,9 @@
 "use client";
 import React from "react";
-import PropTypes from 'prop-types';
-import Box from '@mui/material/Box';
-import Collapse from '@mui/material/Collapse';
-import IconButton from '@mui/material/IconButton';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Typography from '@mui/material/Typography';
-import Paper from '@mui/material/Paper';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+
 import Account from "../my-list/account";
 
 const Orders = () => {
-
   return (
     <div className="bg-gray-200 py-12">
       <div className="container">
@@ -30,72 +16,11 @@ const Orders = () => {
                 products in my List
               </p>
             </div>
-
-            <TableRow sx={{ "& > *": { borderBottom: "unset" } }}>
-              <TableCell>
-                <IconButton
-                  aria-label="expand row"
-                  size="small"
-                  onClick={() => setOpen(!open)}
-                >
-                  {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-                </IconButton>
-              </TableCell>
-              <TableCell component="th" scope="row">
-                gfghgf
-              </TableCell>
-              <TableCell align="right">56456</TableCell>
-              <TableCell align="right">7457</TableCell>
-              <TableCell align="right">577</TableCell>
-              <TableCell align="right">vhj</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell
-                style={{ paddingBottom: 0, paddingTop: 0 }}
-                colSpan={6}
-              >
-                <Collapse in={open} timeout="auto" unmountOnExit>
-                  <Box sx={{ margin: 1 }}>
-                    <Typography variant="h6" gutterBottom component="div">
-                      History
-                    </Typography>
-                    <Table size="small" aria-label="purchases">
-                      <TableHead>
-                        <TableRow>
-                          <TableCell>Date</TableCell>
-                          <TableCell>Customer</TableCell>
-                          <TableCell align="right">Amount</TableCell>
-                          <TableCell align="right">Total price ($)</TableCell>
-                        </TableRow>
-                      </TableHead>
-                      <TableBody>
-                       
-                          <TableRow>
-                            <TableCell component="th" scope="row">
-                              56756
-                            </TableCell>
-                            <TableCell>56756</TableCell>
-                            <TableCell align="right">
-                           6757
-                            </TableCell>
-                            <TableCell align="right">
-                              {Math.round(5757567 * 56756756 * 100) /
-                                100}
-                            </TableCell>
-                          </TableRow>
-                 
-                      </TableBody>
-                    </Table>
-                  </Box>
-                </Collapse>
-              </TableCell>
-            </TableRow>
           </div>
         </div>
       </div>
     </div>
   );
 };
-
 
 export default Orders;
